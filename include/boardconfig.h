aca7e574b989d576600d985a70ded51b626d602e
braelan-creswell <braelan.creswell@16studios.com>
2023-04-07 03:21:19 -0400

    CAN Comms working

--- include/boardconfig.h
+++ include/boardconfig.h
@@ -13,8 +13,8 @@
 #define KLINETX 17
 #define J1850TX 18
 #define J1850RX 19
-#define CANTX 25
-#define CANRX 26
+#define CANTX GPIO_NUM_25
+#define CANRX GPIO_NUM_26
 #define VREFADC 34 //VCC--10K--VREFADC--2.7K--GND
 
 #define BLE_SERVICE_UUID    "e0bf7add-8f80-4c51-b4ed-5d136c6c6837" //"19fdde56-369e-4785-bce9-eed84ecb9999" UART service UUID
